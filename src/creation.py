4269eb8b7aad822a9f71ae22f4593aa93aeacb34
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2018-12-02 22:48:42 +0100

    Creation
    création classe Annonce

--- src/creation.py
+++ src/creation.py
@@ -13,6 +13,8 @@ Fonctions pour la création du jeu de données.
 from selenium import webdriver
 from time import sleep
 from pathlib import Path
+import re
+
 
 class Session:
     """
@@ -37,21 +39,50 @@ class Session:
         self.navigateur.find_element_by_link_text("Suivant").click()
 
 
-def get_genre(annonce):
-    """Fournit le style de l'annonce"""
-    l = annonce.find_element_by_class_name("c-pa-link")
-    return l.text
+class Annonce:
+    """
+    Classe pour la gestion d'une annonce. Le tag de l'annonce ayant une durée de vie limite à
+    celle de la page dans le nagivateur n'est pas stocké dans l'élément.
+    """
+
+    motif = re.compile("(\d+)\sp\s(\d+)\sch\s(\d+).*")
+
+    def __init__(self, annonce):
+        self.set_id(annonce)
+        self.set_genre(annonce)
+        self.set_prix(annonce)
+        self.set_pcs(annonce)
 
 
-def get_id(annonce):
-    """Renvoit l'identifiant unique de l'annonce"""
-    return annonce.get_attribute("id")
+    def set_genre(self, annonce):
+        """Affecte le style de l'annonce"""
+        l = annonce.find_element_by_class_name("c-pa-link")
+        self.genre = l.text
 
 
-def get_bouton(annonce):
-    """Renvoit le lien vers la page détaillée"""
-    b = annonce.find_element_by_class_name("button")
-    return b.find_element_by_tag_name("a")
+    def set_id(self, annonce):
+        """Affecte l'identifiant unique de l'annonce"""
+        self.id = annonce.get_attribute("id")
+
+
+    def set_prix(self, annonce):
+        """Affecte le prix sous la forme d'une chaine de caractères"""
+        t = annonce.find_element_by_class_name("c-pa-price")
+        self.prix = t.text
+
+
+    def set_pcs(self, annonce):
+        """Affecte pieces chambres surface"""
+        c = annonce.find_element_by_class_name("c-pa-criterion")
+        se = Annonce.motif.search(c.text)
+        self.pieces, self.chambres, self.surface = se.groups()
+
+
+    def voir_detail(self, annonce):
+        """Clique le lien vers la page détaillée"""
+        b = annonce.find_element_by_class_name("button")
+        l = b.find_element_by_tag_name("a")
+        l.click()
 
 
 if __name__ == "__main__":
