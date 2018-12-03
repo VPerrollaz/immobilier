b15db83e71bd74c9e4d0adaa1fb5604dfb7b796e
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2018-12-03 01:51:04 +0100

    Création de transformation.py
    pcs to p,c,s

---
+++ src/traitement.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2018 vincent <vincent@vincent-U36SG>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Transformation des données brutes.
+"""
+import re
+
+
+motif = re.compile("(\d+)\sp\s(\d+)\sch\s(\d+).*")
+def pcs_conv(pcs):
+    """De la chaine pcs renvoit le triplet pièces, chambres, surface."""
+    s = motif.search(pcs)
+    return s.groups()
+
+
