5fc4cfba913441f0015d7cab4dcec3f34eec0940
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2018-12-03 14:06:27 +0100

    traitement.py
    ajout de la fonction get_genres

--- src/traitement.py
+++ src/traitement.py
@@ -10,6 +10,7 @@
 Transformation des données brutes.
 """
 import re
+import json
 
 
 motif = re.compile("(\d+)\sp\s(\d+)\sch\s(\d+).*")
@@ -17,5 +18,18 @@ def pcs_conv(pcs):
     """De la chaine pcs renvoit le triplet pièces, chambres, surface."""
     s = motif.search(pcs)
     return s.groups()
+
+
+def get_genres(nom_fichier):
+    """Renvoit l'ensemble de tous les genres contenu dans le fichier codé en json"""
+    genres = set()
+    with open(nom_fichier, "r") as fichier:
+        for ligne in fichier:
+            d = json.loads(ligne)
+            genres.add(d["genre"])
+
+    return genres
+
+
 
 
