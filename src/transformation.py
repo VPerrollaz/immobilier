e1a971d6638b3af5f0ff5c52c227b5786e9c5922
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2018-12-03 21:43:36 +0100

    transformation.py
    ajout du filtre pour ne garder que les pertinentes

--- src/transformation.py
+++ src/transformation.py
@@ -10,6 +10,27 @@
 Transformation des données brutes.
 """
 import re
+import json
+
+def chargement(nom_fichier):
+    """Retourne la liste des dictionnaires correspondant aux annonces"""
+    resultat = list()
+    with open(nom_fichier, "r") as fichier:
+        for ligne in fichier:
+            resultat.append(json.loads(ligne))
+
+    return resultat
+
+
+def filtre(annonces):
+    """Ne garde que les maisons et appartements"""
+    resultat = list()
+    valide = {"Appartement", "Maison / Villa", "Appartement neuf", "Maison / Villa neuve"}
+    for annonce in annonces:
+        if annonce["genre"] in valide: 
+            resultat.append(annonce)
+
+    return resultat
 
 
 motif = re.compile("(\d+)\sp\s(\d+)\sch\s(\d+).*")
@@ -18,5 +39,13 @@ def pcs_conv(pcs):
     s = motif.search(pcs)
     return s.groups()
 
+
+def main():
+    annonces = filtre(chargement("./donnees/brute.json"))
+    return annonces
+
+if __name__ == "__main__":
+    annonces = main()
+    print(len(annonces))
 
 
